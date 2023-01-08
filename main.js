ec4c1f84720879a495861539db2cf9f17092e1ca
sebastoico <sejaramillomo@gmail.com>
2023-01-08 15:59:22 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuIcon = document.querySelector(".menu");
+const orderIcon = document.querySelector(".navbar-shopping-cart");
+
 const desktopMenu = document.querySelector(".desktop-menu");
-const mediaMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-mediaMenu.addEventListener("click", toggleMobileMenu)
+menuIcon.addEventListener("click", toggleMobileMenu);
+orderIcon.addEventListener("click", toggleOrderAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
-  console.log("CAMBIO");
+  aside.classList.add("inactive");
+}
+
+function toggleOrderAside() {
+  aside.classList.toggle("inactive");
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
 }
